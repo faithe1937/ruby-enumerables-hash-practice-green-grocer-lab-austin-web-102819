141cdcba38a9b826bbd5f2ca28354c640864f9c3
Faith Janicek <faithe1937@gmail.com>
2019-10-16 01:18:14 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -16,7 +16,7 @@ def apply_coupons(cart, coupons)
     
     if cart[item]
       if cart[item][:count] >= coupon[:num] && !cart["#{item} W/COUPON"]
-        cart["#{item} W/COUPON"]
+        cart["#{item} W/COUPON"] = 
 end
 
 def apply_clearance(cart)
