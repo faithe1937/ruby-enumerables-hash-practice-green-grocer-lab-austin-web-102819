2109bc0463da22918b9ff3edbe9038b0e415d467
Faith Janicek <faithe1937@gmail.com>
2019-10-16 01:09:13 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -1,7 +1,7 @@
 def consolidate_cart(cart)
   receipt_hash = {}
   cart.each do |element_hash|
-  element_hash.each do|item, attribute_hash|
+  element_hash.each do |item, attribute_hash|
   receipt_hash[item] || = attribute_hash
   receipt_hash[item][:count]? receipt_hash[item][:count] +=1: 
   receipt_hash[item][:count] = 1 
