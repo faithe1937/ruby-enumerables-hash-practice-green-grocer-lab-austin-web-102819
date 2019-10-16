8e85b5fb21b575cd1c120aae79a91d5fe543adbf
Faith Janicek <faithe1937@gmail.com>
2019-10-16 01:46:36 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -37,7 +37,7 @@ end
 
 def checkout(items, coupons)
   cart = consolidate_cart(items)
-  cart1 = apply_coupons(carts,coupons)
+  cart1 = apply_coupons(cart,coupons)
   cart2 = apply_clearance(cart1)
 
 total = 0 
